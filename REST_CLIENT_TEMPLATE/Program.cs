8f87b073871eddf81900bacfb24124f87afa4280
natsachaj <natascha_jakobsen@hotmail.com>
2018-01-01 19:36:42 +0100

    Add project files.

---
+++ REST_CLIENT_TEMPLATE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REST_CLIENT_TEMPLATE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
